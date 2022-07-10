eb0487ae6117e791761ee7db3c82d2c1d9503fe4
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-07-10 17:20:57 +0530

    feat: update deletePost fn

--- hardhat/contracts/Social.sol
+++ hardhat/contracts/Social.sol
@@ -8,10 +8,13 @@ contract Social {
 
     event NewPost(string ipfs, address user);
     event DeletePost(string ipfs, address user);
-    event NewComment(string comment, string postIpfs, address user);
-    event DeleteComment(string comment, string postIpfs);
+
+    // event NewComment(string comment, string postIpfs, address user);
+    // event DeleteComment(string comment, string postIpfs);
 
     function newPost(string memory _ipfs) public {
+        require(bytes(_ipfs).length != 0); // consult on max byte length
+
         posts.push(_ipfs);
         postAuthor[posts.length - 1] = msg.sender;
 
@@ -21,6 +24,8 @@ contract Social {
     function deletePost(uint _id) public {
         require(_id < posts.length);
         require(msg.sender == postAuthor[_id], "msg sender not author of post");
+        postAuthor[_id] = address(0);
+        posts[_id] = "";
 
         emit DeletePost(posts[_id], msg.sender);
     }
