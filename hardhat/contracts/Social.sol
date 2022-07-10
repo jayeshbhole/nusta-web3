fc6afa4d774ef4a0ae3c5046f49738d989cb6f39
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-07-10 17:17:13 +0530

    Create Social.sol

---
+++ hardhat/contracts/Social.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// The Social contract has event emissions for a social feed.
+contract Social {
+    string[] public posts;
+    mapping(uint => address) public postAuthor;
+
+    event NewPost(string ipfs, address user);
+    event DeletePost(string ipfs, address user);
+    event NewComment(string comment, string postIpfs, address user);
+    event DeleteComment(string comment, string postIpfs);
+
+    function newPost(string memory _ipfs) public {
+        posts.push(_ipfs);
+        postAuthor[posts.length - 1] = msg.sender;
+
+        emit NewPost(_ipfs, msg.sender);
+    }
+
+    function deletePost(uint _id) public {
+        require(_id < posts.length);
+        require(msg.sender == postAuthor[_id], "msg sender not author of post");
+
+        emit DeletePost(posts[_id], msg.sender);
+    }
+}
